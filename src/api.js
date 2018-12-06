0c94f03c97253ce96392ed5945f216f1fe0f4bfa
catsigma <im@catsigma.com>
2018-12-06 18:07:41 +0800

    add fetching mehtods for block hash, contract, balance and manager_key

--- src/api.js
+++ src/api.js
@@ -11,11 +11,23 @@ export const gets : ApiGetsFunc = (fetch : GetRPCFunc) => {
     head(sub_key? : string) {
       return fetch(`/chains/main/blocks/head/${sub_key || ''}`)
     },
+    hash() {
+      return mapping.head('hash')
+    },
     header() {
       return mapping.head('header')
     },
     protocol() {
       return mapping.header().then(x => safeProp(x, 'protocol'))
+    },
+    balance(address : string) {
+      return fetch(`/chains/main/blocks/head/context/delegates/${address}/balance`)
+    },
+    contract(address : string) {
+      return fetch(`/chains/main/blocks/head/context/contracts/${address}`)
+    },
+    manager_key(address : string) {
+      return fetch(`/chains/main/blocks/head/context/contracts/${address}/manager_key`).then(x => safeProp(x, 'manager'))
     }
   }
 
--- test/index.js
+++ test/index.js
@@ -18,6 +18,18 @@ const main = async () => {
 
   const r3 : Object = await network_client.fetch.protocol()
   assert(r3.length === 51 && r3[0] === 'P', 'head.header.protocol')
+
+  const r4 : Object = await network_client.fetch.hash()
+  assert(r4.length === 51, 'head.hash')
+
+  const r5 : Object = await network_client.fetch.contract('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
+  assert(r5.script && r5.manager && r5.balance, 'contract')
+
+  const r6 : Object = await network_client.fetch.balance('tz1TUswtLE1cTBgoBC2JAtQ5Jsz2crp1tZvJ')
+  assert(parseInt(r6) > 1, 'contract.balance')
+
+  const r7 : Object = await network_client.fetch.manager_key('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
+  assert(r7 === 'tz1gRaTpXdZPEdcoG85gG9EuUYNeRKuTUoz4', 'contract.manager')
 }
 
 main()
