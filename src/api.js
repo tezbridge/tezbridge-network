b467108a4d237604c844dc2d66d59f4b47d4648b
catsigma <im@catsigma.com>
2018-12-10 14:55:03 +0800

    add fetch methods: counter, predecessor, contract_bytes, storage_bytes, big_map_bytes

--- src/api.js
+++ src/api.js
@@ -5,6 +5,15 @@ import { safeProp } from './types'
 
 export class Gets {
   fetch : GetRPCFunc
+  static filter_hash_url(x : string) {
+    if (x.indexOf('/') === -1)
+      throw "The input hash_url should be in this format: `xx/xx/xx/xx/xx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`"
+
+    const start = x[0] === '/' ? 1 : 0
+    const end = x.slice(-1) === '/' ? -1 : x.length
+
+    return x.slice(start, end)
+  }
 
   constructor(fetch : GetRPCFunc) {
     this.fetch = fetch
@@ -12,8 +21,8 @@ export class Gets {
   custom(path : string) {
     return this.fetch(path)
   }
-  head(sub_key? : string) {
-    return this.fetch(`/chains/main/blocks/head/${sub_key || ''}`)
+  head(sub_path? : string) {
+    return this.fetch(`/chains/main/blocks/head/${sub_path || ''}`)
   }
   hash() {
     return this.head('hash')
@@ -24,16 +33,33 @@ export class Gets {
   protocol() {
     return this.header().then(x => safeProp(x, 'protocol'))
   }
+  predecessor() {
+    return this.header().then(x => safeProp(x, 'predecessor'))
+  }
   balance(address : string) {
     return this.fetch(`/chains/main/blocks/head/context/delegates/${address}/balance`)
   }
   contract(address : string) {
     return this.fetch(`/chains/main/blocks/head/context/contracts/${address}`)
   }
+  contract_bytes(hash_url : string, sub_path? : string) {
+    const hash = Gets.filter_hash_url(hash_url)
+    return this.fetch(`/chains/main/blocks/head/context/raw/bytes/contracts/index/originated/${hash}${sub_path || ''}`)
+  }
+  storage_bytes(hash_url : string) {
+    return this.contract_bytes(hash_url, '/data/storage')
+  }
+  big_map_bytes(hash_url : string) {
+    return this.contract_bytes(hash_url, '/big_map')
+  }
   manager_key(address : string) {
     return this.fetch(`/chains/main/blocks/head/context/contracts/${address}/manager_key`)
            .then(x => safeProp(x, 'manager'))
   }
+  counter(address : string) {
+    return this.fetch(`/chains/main/blocks/head/context/contracts/${address}/counter`)
+  }
+
 }
 
 export class Posts {
--- test/index.js
+++ test/index.js
@@ -1,41 +1,104 @@
 // @flow
 
 import TezBridgeNetwork from './../src/index'
-import { RPCFn, assert } from './util'
+import { Gets } from './../src/api'
 
+import { RPCFn, assert } from './util'
 
 const network_client = new TezBridgeNetwork({
   host: 'https://alphanet.tezrpc.me',
   RPCFn
 })
 
+const fn_tests = async () => {
+  {
+  const r1 = Gets.filter_hash_url('3e/e2/31/36/6b/1336eb61419df8fc666056025929bf')
+  const r2 = Gets.filter_hash_url('/3e/e2/31/36/6b/1336eb61419df8fc666056025929bf')
+  const r3 = Gets.filter_hash_url('/3e/e2/31/36/6b/1336eb61419df8fc666056025929bf/')
+  const r4 = Gets.filter_hash_url('3e/e2/31/36/6b/1336eb61419df8fc666056025929bf/')
+  assert(
+    r1 === r2 && r2 === r3 && r3 === r4 && r1 === '3e/e2/31/36/6b/1336eb61419df8fc666056025929bf', 
+    'FN: filter hash url')
+  }
+}
+
+const gets_tests = async () => {
+  {
+    const r : Object = await network_client.fetch.head()
+    assert(r.header.level > 1, 'FETCH: head.header.level')
+  }
+  
+  {
+    const r : Object = await network_client.fetch.header()
+    assert(r.level > 1, 'FETCH: header.level')
+  }
+
+  {
+    const r : Object = await network_client.fetch.protocol()
+    assert(r.length === 51 && r[0] === 'P', 'FETCH: head.header.protocol')
+  }
+
+  {
+    const r : Object = await network_client.fetch.hash()
+    assert(r.length === 51, 'FETCH: head.hash')
+  }
+
+  {
+    const r : Object = await network_client.fetch.contract('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
+    assert(r.script && r.manager && r.balance, 'FETCH: contract')
+  }
+
+  {
+    const r : Object = await network_client.fetch.balance('tz1TUswtLE1cTBgoBC2JAtQ5Jsz2crp1tZvJ')
+    assert(parseInt(r) > 1, 'FETCH: contract.balance')
+  }
+
+  {
+    const r : Object = await network_client.fetch.manager_key('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
+    assert(r === 'tz1gRaTpXdZPEdcoG85gG9EuUYNeRKuTUoz4', 'FETCH: contract.manager')
+  }
+
+  {
+    const r : Object = await network_client.fetch.counter('tz1TUswtLE1cTBgoBC2JAtQ5Jsz2crp1tZvJ')
+    assert(parseInt(r) > 1, 'FETCH: contract.counter')
+  }
+
+  {
+    const r : Object = await network_client.fetch.predecessor()
+    assert(r.length === 51, 'FETCH: head.header.predecessor')
+  }
+
+  {
+    const r : Object = await network_client.fetch.contract_bytes('3e/e2/31/36/6b/1336eb61419df8fc666056025929bf')
+    assert(r.balance && r.data && r.used_bytes && r.len && r.manager, 'FETCH: raw contract in bytes')
+  }
+
+  {
+    const r : Object = await network_client.fetch.storage_bytes('3e/e2/31/36/6b/1336eb61419df8fc666056025929bf')
+    const storage_len = parseInt(r.slice(0, 8), 16)
+    assert(r.length === 8 + storage_len * 2, 'FETCH: raw storage of contract in bytes')
+  }
+
+  {
+    const r : Object = await network_client.fetch.big_map_bytes('3e/e2/31/36/6b/1336eb61419df8fc666056025929bf')
+    assert(Object.keys(r).reduce((acc, x) => acc && (x.length === 2 ? true : false), true), 'FETCH: raw big_map of contract in bytes')
+  }
+}
+
+const posts_tests = async () => {
+  {
+    const r : Object = await network_client.submit.pack_data(
+      { "prim": "Pair", "args": [ { "string": "abc" }, { "int": "34" } ] }, 
+      { "prim": "pair", "args": [ { "prim": "string" }, { "prim": "nat" } ] }
+    )
+    assert(r === '05070701000000036162630022', 'SUBMIT: pack data')
+  }
+}
+
 const main = async () => {
-  const r1 : Object = await network_client.fetch.head()
-  assert(r1.header.level > 1, 'FETCH: head.header.level')
-
-  const r2 : Object = await network_client.fetch.header()
-  assert(r2.level > 1, 'FETCH: header.level')
-
-  const r3 : Object = await network_client.fetch.protocol()
-  assert(r3.length === 51 && r3[0] === 'P', 'FETCH: head.header.protocol')
-
-  const r4 : Object = await network_client.fetch.hash()
-  assert(r4.length === 51, 'FETCH: head.hash')
-
-  const r5 : Object = await network_client.fetch.contract('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
-  assert(r5.script && r5.manager && r5.balance, 'FETCH: contract')
-
-  const r6 : Object = await network_client.fetch.balance('tz1TUswtLE1cTBgoBC2JAtQ5Jsz2crp1tZvJ')
-  assert(parseInt(r6) > 1, 'FETCH: contract.balance')
-
-  const r7 : Object = await network_client.fetch.manager_key('KT1Ap4UZRBGGo9Pm92sFbR8gJrrFTfUbX4kc')
-  assert(r7 === 'tz1gRaTpXdZPEdcoG85gG9EuUYNeRKuTUoz4', 'FETCH: contract.manager')
-
-  const r8 : Object = await network_client.submit.pack_data(
-    { "prim": "Pair", "args": [ { "string": "abc" }, { "int": "34" } ] }, 
-    { "prim": "pair", "args": [ { "prim": "string" }, { "prim": "nat" } ] }
-  )
-  assert(r8 === '05070701000000036162630022', 'SUBMIT: pack data')
+  await fn_tests()
+  await gets_tests()
+  await posts_tests()
 }
 
 main()
